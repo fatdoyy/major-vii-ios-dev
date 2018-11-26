b1b36764f0118753a29edeea70f229179d0a46f7
jason <jason.chan@modeveapp.com>
2018-11-26 19:44:30 +0800

    Fixed featured cell bookmark action; EventDetailsView (20%)

---
+++ Events/EventDetailsView.swift
@@ -0,0 +1,21 @@
+//
+//  EventDetailsView.swift
+//  major-7-ios
+//
+//  Created by jason on 26/11/2018.
+//  Copyright © 2018 Major VII. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
